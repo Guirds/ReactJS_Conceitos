4b9afaa0e0c5a81c8295a2c0dd3d299d0badd2ff
Guilherm Santos <gui07015@gmail.com>
2019-05-02 18:04:30 -0300

    Propriedades/Children

---
+++ exemplos/exemplo1.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+class App extends Component {
+  handleClick() {
+    alert("Botão clicado");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rocketseat</h1>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
