0099b5f5d7ed6bc924f5f8e89818fe8136ad180e
Guilherm Santos <gui07015@gmail.com>
2019-05-02 18:06:21 -0300

    Estado e imutabilidade

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { render } from "react-dom";
 
 class Button extends Component {
+  static defaultProps = {
+    //Definindo as propriedades padrão do Children
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired, //Definindo os parametros do onclick, o que ele é? isRequired(obrigatorio a inserção de algum valor)
+    children: PropTypes.string //É uma string
+  };
+
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
 class App extends Component {
-  handleClick() {
-    alert("Botão clicado");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
   render() {
     return (
       <Fragment>
         <h1>Hello Rocketseat</h1>
-        <Button onClick={this.handleClick}>Enviar</Button>
+        <h2>{this.state.counter}</h2>
+        <Button
+          onClick={() => {
+            alert("Button 1");
+          }}
+        />
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
